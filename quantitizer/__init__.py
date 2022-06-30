365b72918bc808f4711d072162c7297f87d976d9
CapBlood <stalker.anonim@mail.ru>
2022-06-30 17:22:19 +0300

    fix(квантование): исправлена квантование на CUDA

--- quantitizer/__init__.py
+++ quantitizer/__init__.py
@@ -29,7 +29,7 @@ def quantitize(vectors, sub_size=8, n_cluster=256, n_iter=20, minit='points', se
         n_cluster, indexes, code_books)
 
 
-def quantitize_experimental(vectors, sub_size=8, sample=None, n_cluster=256, n_iter=20, minit='points', seed=123):
+def quantitize_experimental(vectors, sub_size=8, n_cluster=256, n_iter=20, seed=123):
     if len(vectors[0]) % sub_size != 0:
         raise Exception(f"sub_size должен нацело делить {len(vectors[0])}")
 
@@ -41,13 +41,13 @@ def quantitize_experimental(vectors, sub_size=8, sample=None, n_cluster=256, n_i
         # centroid, label = kmeans2(
         #     part, n_cluster, n_iter, minit=minit, seed=seed)
         kmeans_float = KMeans(
-            n_clusters=n_cluster, max_iter=10)
+            n_clusters=n_cluster, max_iter=n_iter)
         kmeans_float.fit(part)
         code_books.append(kmeans_float.cluster_centers_.tolist())
         indexes.append(kmeans_float.labels_.tolist())
 
-    code_books = np.array(code_books)
-    indexes = np.array(indexes).T
+    code_books = np.array(code_books, dtype=np.float16)
+    indexes = np.array(indexes, dtype=np.int8).T
 
     # return code_books, np.array(indexes).T
     return PQ(

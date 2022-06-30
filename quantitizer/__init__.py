74eec5ead9be522ea7bde46e830d2dbd59eabd0b
CapBlood <stalker.anonim@mail.ru>
2022-06-30 16:57:40 +0300

    feat(квантование): добавлен вариант квантования с помощью CUDA

--- quantitizer/__init__.py
+++ quantitizer/__init__.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.cluster.vq import kmeans2
+from cuml import KMeans
+from cuml.cluster import KMeans
 
 from quantitizer._pq_array import PQ
 
@@ -28,22 +30,26 @@ def quantitize(vectors, sub_size=8, n_cluster=256, n_iter=20, minit='points', se
 
 
 def quantitize_experimental(vectors, sub_size=8, sample=None, n_cluster=256, n_iter=20, minit='points', seed=123):
-    encoder = pqkmeans.encoder.PQEncoder(
-        iteration=n_iter,
-        num_subdim=sub_size,
-        Ks=n_cluster
-    )
+    if len(vectors[0]) % sub_size != 0:
+        raise Exception(f"sub_size должен нацело делить {len(vectors[0])}")
 
-    if sample is None:
-        selection = vectors
-    else:
-        indexes = np.random.randint(vectors, size=sample)
-        selection = vectors[indexes]
+    np.random.seed(seed)
+    parts = np.array_split(vectors, sub_size, axis=1)
 
-    encoder.fit(selection)
-    indexes = encoder.transform(vectors).astype(PQ.index_type(n_cluster))
-    codes = encoder.codewords
+    code_books, indexes = [], []
+    for part in parts:
+        # centroid, label = kmeans2(
+        #     part, n_cluster, n_iter, minit=minit, seed=seed)
+        kmeans_float = KMeans(
+            n_clusters=n_cluster, max_iter=10)
+        kmeans_float.fit(part)
+        code_books.append(kmeans_float.cluster_centers_.tolist())
+        indexes.append(kmeans_float.labels_.tolist())
 
+    code_books = np.array(code_books)
+    indexes = np.array(indexes).T
+
+    # return code_books, np.array(indexes).T
     return PQ(
         len(vectors), len(vectors[0]), sub_size,
-        n_cluster, indexes, codes)
+        n_cluster, indexes, code_books)
